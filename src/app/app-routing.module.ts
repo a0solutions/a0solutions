37249302625f4e683ad631de77048d4c9641500d
A0Solutions <150471177+a0solutions@users.noreply.github.com>
2023-12-07 12:07:24 -0500

    Creating structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemplateComponent } from './template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: TemplateComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
